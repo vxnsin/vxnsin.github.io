1372233170a0f7e98470fdf6bf9eafb5dbf1ef73
Vensin <luiskanzewitsch@gmail.com>
2024-11-01 16:25:20 +0100

    + About Me: Discord Activity (Currently has some bugs) + About Me: Update every 60 seconds + About Me: Rewrite to JQuery + About Me: Mute Button + Links: Discord + Links: Updaet to my Redirect links

--- about/script.js
+++ about/script.js
@@ -1,6 +1,94 @@
-// function([string1, string2],target id,[color1,color2])    
-window.addEventListener('load', updateStatus);
-consoleText(['Vensin', 'About Me'], 'text',['white','white']);
+let currentActivityIndex = 0;
+
+$(document).ready(function() {
+  updateStatus();
+  consoleText(['Vensin', 'About Me'], 'text', ['white', 'white']);
+  
+  const muteButton = $("#muteButton");
+  const audio = $("#myAudio")[0];
+
+  if (muteButton.length && audio) {
+      muteButton.on("click", function() {
+          audio.muted = !audio.muted;
+          $(this).html(audio.muted 
+              ? '<i class="fas fa-volume-mute icon"></i>' 
+              : '<i class="fas fa-volume-up icon"></i>'
+          );
+      });
+  } else {
+      console.error("Mute Button or Audio Element not found.");
+  }
+
+  const activityIcon = $("#activity-icon");
+  const activityPopup = $("#activity-popup");
+
+  if (activityIcon.length && activityPopup.length) {
+      activityIcon.on("click", function() {
+          const currentActivity = $(this).data("activity");
+          if (currentActivity) {
+              activityPopup.text(currentActivity);
+              activityPopup.css({opacity: 0, display: 'block'});
+              setTimeout(() => {
+                  activityPopup.animate({opacity: 1}, 100);
+              }, 100);
+              setTimeout(() => {
+                  activityPopup.animate({opacity: 0}, 500, () => {
+                      activityPopup.css('display', 'none');
+                  });
+              }, 5000);
+          }
+      });
+  } else {
+      console.error("Activity icon or popup element not found");
+  }
+
+  const musicIcon = $("#music-icon");
+  const songPopup = $("#song-popup");
+
+  if (musicIcon.length && songPopup.length) {
+      musicIcon.on("click", function() {
+          const currentSong = $(this).data("song");
+          const songLink = $(this).data("song-link");
+          if (currentSong) {
+              songPopup.html(`<a href="${songLink}" target="_blank" style="color: white; text-decoration: none;">${currentSong}</a>`);
+              songPopup.css({opacity: 0, display: 'block'});
+              setTimeout(() => {
+                  songPopup.animate({opacity: 1}, 100);
+              }, 100);
+              setTimeout(() => {
+                  songPopup.animate({opacity: 0}, 500, () => {
+                      songPopup.css('display', 'none');
+                  });
+              }, 5000);
+          }
+      });
+  } else {
+      console.error("Music icon or song popup element not found");
+  }
+
+  const statusIcon = $("#status-icon");
+  const statusPopup = $("#status-popup");
+
+  if (statusIcon.length && statusPopup.length) {
+      statusIcon.on("click", function() {
+          const currentState = $(this).data("state");
+          if (currentState) {
+              statusPopup.text(currentState);
+              statusPopup.css({opacity: 0, display: 'block'});
+              setTimeout(() => {
+                  statusPopup.animate({opacity: 1}, 100);
+              }, 100);
+              setTimeout(() => {
+                  statusPopup.animate({opacity: 0}, 500, () => {
+                      statusPopup.css('display', 'none');
+                  });
+              }, 5000);
+          }
+      });
+  } else {
+      console.error("Status icon or popup element not found");
+  }
+});
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
@@ -9,61 +97,56 @@ function consoleText(words, id, colors) {
   var letterCount = 1;
   var x = 1;
   var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
+  var target = document.getElementById(id);
+  target.setAttribute('style', 'color:' + colors[0]);
+  
   window.setInterval(function() {
-
-    if (letterCount === 0 && waiting === false) {
-      waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
-        var usedColor = colors.shift();
-        colors.push(usedColor);
-        var usedWord = words.shift();
-        words.push(usedWord);
-        x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
-        letterCount += x;
-        waiting = false;
-      }, 1000)
-    } else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function() {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-      }, 1000)
-    } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
-      letterCount += x;
-    }
-  }, 120)
+      if (letterCount === 0 && waiting === false) {
+          waiting = true;
+          target.innerHTML = words[0].substring(0, letterCount);
+          window.setTimeout(function() {
+              var usedColor = colors.shift();
+              colors.push(usedColor);
+              var usedWord = words.shift();
+              words.push(usedWord);
+              x = 1;
+              target.setAttribute('style', 'color:' + colors[0]);
+              letterCount += x;
+              waiting = false;
+          }, 1000);
+      } else if (letterCount === words[0].length + 1 && waiting === false) {
+          waiting = true;
+          window.setTimeout(function() {
+              x = -1;
+              letterCount += x;
+              waiting = false;
+          }, 1000);
+      } else if (waiting === false) {
+          target.innerHTML = words[0].substring(0, letterCount);
+          letterCount += x;
+      }
+  }, 120);
+  
   window.setInterval(function() {
-    if (visible === true) {
-      con.className = 'underscore hidden'
-      visible = false;
-
-    } else {
-      con.className = 'underscore'
-
-      visible = true;
-    }
-  }, 400)
+      if (visible === true) {
+          con.className = 'underscore hidden';
+          visible = false;
+      } else {
+          con.className = 'underscore';
+          visible = true;
+      }
+  }, 400);
 }
-function removeBlur() {
-    var blurScreen = document.getElementById('blur-screen');
-    blurScreen.style.display = 'none';
-    var containerMain = document.getElementById('container--main');
-    containerMain.classList.remove('hidden');
-    containerMain.style.opacity = '1';
-    containerMain.style.transform = 'translateY(0)';
-    var audio = document.getElementById('myAudio');
-    
-    audio.volume = 0.2; 
 
-    audio.loop = true; 
-    
-    audio.play();
+function removeBlur() {
+  $('#blur-screen').fadeOut(300);
+  const containerMain = $('#container--main');
+  containerMain.removeClass('hidden').css({opacity: '1', transform: 'translateY(0)'});
+  
+  const audio = $("#myAudio")[0];
+  audio.volume = 0.2; 
+  audio.loop = true; 
+  audio.play();
 }
 
 async function fetchDiscordStatus() {
@@ -78,101 +161,69 @@ async function fetchDiscordStatus() {
 
 async function updateStatus() {
   const discordData = await fetchDiscordStatus();
+  const activityIcon = $("#activity-icon");
+  const musicIcon = $("#music-icon");
+  const statusIcon = $("#status-icon");
+  const statusIndicator = $('#status-indicator');
 
   if (discordData && discordData.success) {
-      const userStatus = discordData.data.discord_status; 
-      const customStatus = discordData.data.activities.find(activity => activity.type === 4)?.state; 
-      const isListeningToSpotify = discordData.data.listening_to_spotify; 
-      const currentSong = isListeningToSpotify ? discordData.data.spotify.song : null; 
-      const currentSongID = isListeningToSpotify ? discordData.data.spotify.track_id : null;  
+      const userStatus = discordData.data.discord_status;
+      const isListeningToSpotify = discordData.data.listening_to_spotify;
+      const activities = discordData.data.activities;
+      const customStatus = discordData.data.activities.find(activity => activity.type === 4)?.state;
 
-      const statusIndicator = document.getElementById('status-indicator');
-      const musicIcon = document.getElementById('music-icon');
-      const statusIcon = document.getElementById('status-icon');
+      statusIndicator.removeClass('status-online status-dnd status-idle status-offline').addClass(`status-${userStatus}`);
 
-      // Update den Status-Indicator
-      statusIndicator.classList.remove('status-online', 'status-dnd', 'status-idle', 'status-offline'); 
-
-      if (userStatus === "online") {
-          statusIndicator.classList.add('status-online');
-      } else if (userStatus === "dnd") { 
-          statusIndicator.classList.add('status-dnd'); 
-      } else if (userStatus === "idle") { 
-          statusIndicator.classList.add('status-idle'); 
+      if (isListeningToSpotify) {
+          musicIcon.addClass("rotating").css('display', 'block');
+          const currentSong = discordData.data.spotify.song;
+          const songID = discordData.data.spotify.track_id;
+          musicIcon.attr("data-song", currentSong).attr("data-song-link", `https://open.spotify.com/track/${songID}`);
       } else {
-          statusIndicator.classList.add('status-offline'); 
-      }
-
-    
-        if (isListeningToSpotify) {
-          musicIcon.classList.add("rotating");
-          musicIcon.style.display = 'block'; 
-          musicIcon.setAttribute("data-song", currentSong);
-          musicIcon.setAttribute("data-song", currentSong);
-          const songLink = currentSongID ? `https://open.spotify.com/track/${currentSongID}` : null;
-          
-          if (songLink) {
-              musicIcon.setAttribute("data-song-link", songLink); 
-          }
-      } else {
-          musicIcon.classList.remove("rotating");
-          musicIcon.style.display = 'none'; 
+          musicIcon.removeClass("rotating").css('display', 'none');
       }
 
       if (customStatus) {
-          statusIcon.style.display = 'block'; 
-          statusIcon.setAttribute("data-state", customStatus);
+          statusIcon.show();
+          statusIcon.attr("data-state", customStatus); 
       } else {
-          statusIcon.style.display = 'none'; 
+          statusIcon.hide(); 
+      }
+
+      const filteredActivities = activities.filter(activity => 
+          activity.type !== 4 && 
+          !(activity.type === 1 && activity.name === "Spotify") 
+      );
+
+      const activityCount = filteredActivities.length;
+
+      if (activityCount > 0) {
+          const activity = filteredActivities[currentActivityIndex];
+          let activityText = '';
+          let emoji = '';
+
+          switch (activity.name) {
+              case "AniWorld":
+                  emoji = '🍿';
+                  activityText = `Anime - ${activity.details}`;
+                  break;
+              case "Visual Studio Code":
+                  emoji = '⌨️';
+                  const filteredDetails = activity.state.replace(/💼/g, '').replace(/ᕁ/g, '').trim();
+                  activityText = `Working - ${filteredDetails}`;
+                  break;
+          }
+
+          activityIcon.css('display', 'block') 
+              .attr("data-activity", activityText) 
+              .text(emoji);
+
+          currentActivityIndex = (currentActivityIndex + 1) % filteredActivities.length;
+          activityIcon.attr("data-index", currentActivityIndex);
+      } else {
+          activityIcon.css('display', 'none'); 
       }
   }
 }
 
-document.getElementById("music-icon").addEventListener("click", () => {
-  const musicIcon = document.getElementById("music-icon");
-  const songPopup = document.getElementById("song-popup");
-  const currentSong = musicIcon.getAttribute("data-song");
-  const songLink = musicIcon.getAttribute("data-song-link");
-
-  if (currentSong) {
-    songPopup.innerHTML = `<a href="${songLink}" target="_blank" style="color: white; text-decoration: none;">${currentSong}</a>`;
-      songPopup.style.opacity = 0; 
-      songPopup.style.display = 'block'; 
-      
-      setTimeout(() => {
-        songPopup.style.opacity = 1; 
-    }, 100); 
-
-
-      setTimeout(() => {
-          songPopup.style.opacity = 0; 
-          setTimeout(() => {
-              songPopup.style.display = 'none'; 
-          }, 500); 
-      }, 5000); 
-  }
-});
-document.getElementById("status-icon").addEventListener("click", () => {
-  const statusIcon = document.getElementById("status-icon");
-  const statusPopup = document.getElementById("status-popup");
-  const currentState = statusIcon.getAttribute("data-state");
-
-  if (currentState) {
-      statusPopup.innerText = `${currentState}`;
-      statusPopup.style.opacity = 0; 
-      statusPopup.style.display = 'block'; 
-      
-      setTimeout(() => {
-        statusPopup.style.opacity = 1; 
-    }, 100);
-
-      setTimeout(() => {
-          statusPopup.style.opacity = 0; 
-          setTimeout(() => {
-              statusPopup.style.display = 'none'; 
-          }, 500); 
-      }, 5000); 
-  }
-});
-
-setInterval(updateStatus, 10000);
+setInterval(updateStatus, 60000);
--- links/app.js
+++ links/app.js
@@ -1,99 +1,95 @@
-const container = document.querySelector('.links-container');
+$(document).ready(function() {
+    const container = $('.links-container');
 
-container.addEventListener('click', function(event) {
-    const shareButton = event.target.closest('.title-share-button');
-    if (shareButton) {
-        event.preventDefault(); 
-        const link = shareButton.getAttribute('link');
+    container.on('click', '.title-share-button', function(event) {
+        event.preventDefault();
+        const link = $(this).attr('link');
         if (isMobileDevice()) {
             shareViaWebAPI(link);
         } else {
             copyText(link);
         }
-    }
-});
-
-async function copyText(link) {
-    try {
-        await navigator.clipboard.writeText(link);
-        alert("Link copied to clipboard: " + link);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-function shareViaWebAPI(link) {
-    navigator.share({
-        title: 'Share Link',
-        text: '👋 - Check out this link:',
-        url: link,
-    }).then(() => {
-        console.log('Link shared successfully');
-    }).catch((error) => {
-        console.error('Error sharing link:', error);
     });
-}
 
-
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'links.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == 200) {
-            callback(JSON.parse(xobj.responseText));
+    async function copyText(link) {
+        try {
+            await navigator.clipboard.writeText(link);
+            alert("Link copied to clipboard: " + link);
+        } catch (err) {
+            console.error(err);
         }
-    };
-    xobj.send(null);
-}
+    }
 
-function createLinks(links) {
-    const container = document.querySelector('.links-container');
+    function isMobileDevice() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
 
-    links.forEach(link => {
-        const title = document.createElement('a');
-        title.classList.add('title');
-        title.setAttribute('href', link.url);
+    function shareViaWebAPI(link) {
+        navigator.share({
+            title: 'Share Link',
+            text: '👋 - Check out this link:',
+            url: link,
+        }).then(() => {
+            console.log('Link shared successfully');
+        }).catch((error) => {
+            console.error('Error sharing link:', error);
+        });
+    }
 
-        const icon = document.createElement('i');
-        icon.classList.add('fab', link.icon, 'fa-2x');
+    function loadJSON(callback) {
+        $.getJSON('links.json', function(data) {
+            callback(data);
+        }).fail(function(jqxhr, textStatus, error) {
+            console.error('Error loading JSON:', textStatus, error);
+        });
+    }
 
-        const text = document.createElement('p');
-        text.textContent = link.name;
+    function createLinks(links) {
+        const container = $('.links-container');
+        links.forEach(link => {
+            const title = $('<a>', {
+                class: 'title',
+                href: link.url
+            });
 
-        const shareButton = document.createElement('div');
-        shareButton.classList.add('title-share-button');
-        shareButton.setAttribute('link', link.url);
+            const icon = $('<i>', {
+                class: 'fab ' + link.icon + ' fa-2x'
+            });
 
-        if (link.shareButton) {
-            const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-            svg.setAttribute('width', '16');
-            svg.setAttribute('height', '16');
-            svg.setAttribute('viewBox', '0 0 16 16');
-            svg.setAttribute('enable-background', 'new 0 0 24 24');
-            svg.classList.add('sc-gKsewC', 'iPWGYb');
+            const text = $('<p>').text(link.name);
 
-            const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            path.setAttribute('fill-rule', 'evenodd');
-            path.setAttribute('clip-rule', 'evenodd');
-            path.setAttribute('d', 'M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z');
-            path.setAttribute('fill', 'currentColor');
+            const shareButton = $('<div>', {
+                class: 'title-share-button',
+                link: link.url
+            });
 
-            svg.appendChild(path);
-            shareButton.appendChild(svg);
-        }
+            if (link.shareButton) {
+                const svg = $('<svg>', {
+                    width: 16,
+                    height: 16,
+                    viewBox: '0 0 16 16',
+                    class: 'sc-gKsewC iPWGYb'
+                });
 
-        title.appendChild(icon);
-        title.appendChild(text);
-        title.appendChild(shareButton);
-        container.appendChild(title);
+                const path = $('<path>', {
+                    'fill-rule': 'evenodd',
+                    'clip-rule': 'evenodd',
+                    d: 'M10.6464 3.85347L11 4.20702L11.7071 3.49992L11.3536 3.14636L8.35355 0.146362H7.64645L4.64645 3.14636L4.29289 3.49992L5 4.20702L5.35355 3.85347L7.5 1.70702V9.49992V9.99992H8.5V9.49992V1.70702L10.6464 3.85347ZM1 5.49994L1.5 4.99994H4V5.99994H2V14.9999H14V5.99994H12V4.99994H14.5L15 5.49994V15.4999L14.5 15.9999H1.5L1 15.4999V5.49994Z',
+                    fill: 'currentColor'
+                });
+
+                svg.append(path);
+                shareButton.append(svg);
+            }
+
+            title.append(icon);
+            title.append(text);
+            title.append(shareButton);
+            container.append(title);
+        });
+    }
+
+    loadJSON(function(response) {
+        createLinks(response);
     });
-}
-
-loadJSON(function(response) {
-    createLinks(response);
 });
